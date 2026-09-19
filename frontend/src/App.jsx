5d9f9ae21ed301c8e0ded9bafada4d6abcb7f086
Sergey <sh@S-M5-Air.local>
2026-09-19 23:45:14 +0400

    zero point

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="app">
+      <div className="sphere">
+        <div className="core"></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
